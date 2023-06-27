36719a15a2ca9e797fd7ad8f08241c868f45418e
stefanelloisaac <i.stefanello@unochapeco.edu.br>
2023-06-27 14:34:04 -0300

    first commit

---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+export default function ({ $axios }, inject) {
+  const api = $axios.create()
+
+  api.onRequest(() => {
+    const token = localStorage.getItem('crsStore-api-token') || ''
+    api.setHeader('Authorization', `Bearer ${token}`)
+  })
+
+  api.onResponse((response) => response.data)
+
+  api.setBaseURL('http://localhost:3333')
+
+  inject('api', api)
+}
